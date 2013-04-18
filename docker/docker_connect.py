7315e0137ebfd0e89b006932fe1d365d6a705e1d
MatApple <mat.apple@gmail.com>
2013-04-18 08:33:19 -0700

    trying a new port forwarder listening on 7000

--- docker/docker_connect.py
+++ docker/docker_connect.py
@@ -19,20 +19,34 @@ def closed_callback():
     print "called back"
 
 def forward(source, dest, cb = lambda: None):
-    """Forwards bytes unidirectionally from source to dest"""
-    while True:
-        d = source.recv(32384)
-        if d == '':
-            cb()
-            break
-        dest.sendall(d)
+	"""Forwards bytes unidirectionally from source to dest"""
+	while True:
+		d = source.recv(32384)
+		print "forwarding to docker: ", d
+		if d == '':
+			cb()
+			break
+		dest.sendall(d)
+
+def callback(source, dest, cb = lambda: None):
+	"""Forwards bytes unidirectionally from source to dest"""
+	while True:
+		d = source.recv(32384)
+		print "receiving from docker: ", d
+		if d == '':
+			cb()
+			break
+		dest.sendall(d)
 
 listener = eventlet.listen((socket.gethostname(), 7000))
 
-while True:
-    client, addr = listener.accept()
-    server = eventlet.connect(('127.0.0.1', 4242))
-    # two unidirectional forwarders make a bidirectional one
-    eventlet.spawn_n(forward, client, server, closed_callback)
-    eventlet.spawn_n(forward, server, client)
+try:
+	while True:
+	    client, addr = listener.accept()
+	    server = eventlet.connect(('127.0.0.1', 4242))
+	    # two unidirectional forwarders make a bidirectional one
+	    eventlet.spawn_n(callback, client, server, closed_callback)
+	    eventlet.spawn_n(forward, server, client)
+except KeyboardInterrupt:
+	sys.exit(0)
 
---
+++ docker/node_connect.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+docker_connect.py
+
+Created by Mat Appelman on 2013-04-17.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+import socket
+import sys
+import simplejson
+import binascii
+import pickle
+
+def bin(x):
+    if x==0:
+        return '0'
+    else:
+        return (bin(x/2)+str(x%2)).lstrip('0') or '0'
+
+
+def docker_connect():
+	HOST, PORT = "ec2-23-20-84-18.compute-1.amazonaws.com", 7000
+	data = "docker ps\n"
+	
+	# Create a socket (SOCK_STREAM means a TCP socket)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	# Connect to server and send data
+	sock.connect((HOST, PORT))
+	sock.send(data)
+	
+	# Receive data from the server and shut down
+	received = sock.recv(1024)
+	sock.close()
+	
+	print("Sent:     %s" % data)
+	print("Received: %s" % received)
+	
+	return
+
+
+if __name__ == '__main__':
+	docker_connect()
