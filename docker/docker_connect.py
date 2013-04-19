cd95806e9ff99aac23d19a65156875adef1d8ac5
MatApple <mat.apple@gmail.com>
2013-04-19 09:48:33 -0700

    updating docker_connect to read stdout pipe correctly

--- docker/docker_connect.py
+++ docker/docker_connect.py
@@ -14,52 +14,68 @@ import sys
 import sys
 from subprocess import PIPE, STDOUT, Popen, CalledProcessError
 from threading  import Thread
-import zerorpc
 
 import eventlet
-"""
-try:
-    from Queue import Queue, Empty
-except ImportError:
-    from queue import Queue, Empty  # python 3.x
-"""
+
 ON_POSIX = 'posix' in sys.builtin_module_names
+
+
 
 
 def closed_callback():
     print "called back"
 
-def enqueue_output(out, queue, proc):
-	while proc.poll() is None:
+
+
+def enqueue_output(out, queue, proc, killq):
+	killsig=False
+	while proc.poll() is None and killsig=False:
 		for line in iter(out.readline, ""):
 			if len(line) > 0:
 				queue.put(line)
+		try:
+			sig = killq.get_nowait() # or q.get(timeout=.1)
+		except:
+			pass
+		else:
+			if sig="KILL":
+				killsig=True
+				break
 		eventlet.sleep(0.1)
-	queue.put("closed connection")
 	out.close()
+	queue.put("closed connection")
+
+
 
 class Docker(object):
 	def __init__(self, client, cb):
 		self.q=eventlet.queue.LightQueue()
+		self.killq=eventlet.queue.LightQueue()
 		self.client=client
+		self.thread=None
+		self.proc=None
+		self.killed=False
 		self.cb=cb
 		self.StdIn()
 		
 	def StdIn(self):
-	    while True:
-	        d = self.client.recv(32384)
-	        if d == '':
-	            self.cb()
-	            break
-	        self.runCommand(d)
+		while True:
+			d = self.client.recv(32384)
+			## listen for closed client connections and hit kill
+			if need_to_kill: self.kill_and_close()
+			if d == '':
+				self.cb()
+				break
+			self.runCommand(d)
+		return
 
 	def runCommand(self,cmd):
 		print "command: ",cmd
-		p = Popen([cmd], stdout=PIPE, stderr=STDOUT, shell=True, close_fds=ON_POSIX)
-		t = Thread(target=enqueue_output, args=(p.stdout, self.q, p))
-		t.daemon = True # thread dies with the program
-		t.start()
+		self.proc = Popen([cmd], stdout=PIPE, stderr=STDOUT, shell=True, close_fds=ON_POSIX)
+		self.thread = Thread(target=enqueue_output, args=(p.stdout, self.q, self.proc, self.killq))
+		self.thread.start()
 		self.stdOut()
+		return
 	
 	def stdOut(self):
 		while True:
@@ -68,9 +84,22 @@ class Docker(object):
 			except:
 				eventlet.sleep(0.1)
 			else:
-				self.client.sendall(msg)
+				try:
+					self.client.sendall(msg)
+				except:
+					if not self.killed: self.kill_and_close()
 				if msg == "closed connection" and self.q.empty():
 					break
+				if msg == "closed connection" and self.killed:
+					break
+		return
+	
+	def kill_and_close(self):
+		if self.proc: self.proc.kill()
+		self.killq.put("KILL")
+		self.killed=True
+		return
+
 			
 
 
